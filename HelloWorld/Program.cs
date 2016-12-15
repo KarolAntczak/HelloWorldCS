d2dfee54c718a2ba759a0232c49e2a827a12ceee
Karol Antczak <k.antczak@samsung.com>
2016-12-15 10:43:52 +0100

    Added Log4Net dependency.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,9 +8,13 @@ namespace HelloWorld
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
+            log.Info("Logger!");
         }
     }
 }
